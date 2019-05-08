6ee0fcfd73af4e6e62175d977081af3b2c9d077c
Shivan Modha <shivanmodha@gmail.com>
2019-05-08 08:17:31 -0400

    Chess Board from State String

--- app/src/main/java/ru/cs213/chess/Board.java
+++ app/src/main/java/ru/cs213/chess/Board.java
@@ -1,11 +1,17 @@
 package ru.cs213.chess;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.drawable.VectorDrawable;
+import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 public class Board extends View {
@@ -25,6 +31,8 @@ public class Board extends View {
      *
      * Therefore, a match history is essentially simply an array of strings
      */
+    // Static variables for class differentiation
+    private static String TAG = "[Board]";
     // Variables to maintain board state
     private String mBoardState = "9BA87AB9CCCCCCCC000000000000000000000000000000006666666635421453";
     /*
@@ -42,6 +50,11 @@ public class Board extends View {
     // Variables to maintain the view rendering state
     private int mWidth = 300;
     private int mHeight = 300;
+    private Paint paintBlack;
+    private Paint paintWhite;
+    private Paint paintTransparent;
+    private Bitmap[] mPieceImages = new Bitmap[13];
+    private boolean mInitializeFailed = false;
     public Board(Context context, AttributeSet attrs) {
         super(context, attrs);
         TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.Board,0, 0);
@@ -49,6 +62,53 @@ public class Board extends View {
 
         } finally {
             a.recycle();
+        }
+        initializePaint();
+        
+    }
+    private void initializePaint() {
+        paintBlack = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paintBlack.setColor(Color.argb(50, 100, 100, 100));
+        paintWhite = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paintWhite.setColor(Color.argb(0, 235, 235, 235));
+        paintTransparent = new Paint(Paint.ANTI_ALIAS_FLAG);
+    }
+    private void initializeImages() {
+        Resources res = getResources();
+        mPieceImages[0] = null;
+        try {
+            mPieceImages[1] = generateBitmap(R.drawable.cp_1);
+            mPieceImages[2] = generateBitmap(R.drawable.cp_2);
+            mPieceImages[3] = generateBitmap(R.drawable.cp_3);
+            mPieceImages[4] = generateBitmap(R.drawable.cp_4);
+            mPieceImages[5] = generateBitmap(R.drawable.cp_5);
+            mPieceImages[6] = generateBitmap(R.drawable.cp_6);
+            mPieceImages[7] = generateBitmap(R.drawable.cp_7);
+            mPieceImages[8] = generateBitmap(R.drawable.cp_8);
+            mPieceImages[9] = generateBitmap(R.drawable.cp_9);
+            mPieceImages[10] = generateBitmap(R.drawable.cp_10);
+            mPieceImages[11] = generateBitmap(R.drawable.cp_11);
+            mPieceImages[12] = generateBitmap(R.drawable.cp_12);
+            mInitializeFailed = false;
+        } catch (Exception e) {
+            mInitializeFailed = true;
+        }
+    }
+    private Bitmap generateBitmap(int drawableId) {
+        int width = mWidth / 8 - 10;
+        int height = mHeight / 8 - 10;
+        VectorDrawable drawable = (VectorDrawable) ContextCompat.getDrawable(getContext(), drawableId);
+        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+        return bitmap;
+    }
+    private int getPieceTypeFromCharIndex(int idx) {
+        try {
+            return Integer.parseInt(mBoardState.charAt(idx) + "", 16);
+        } catch (Exception e) {
+            return 0;
         }
     }
     @Override
@@ -61,19 +121,35 @@ public class Board extends View {
         mHeight = h;
         setMeasuredDimension(w, h);
     }
-
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        paint.setColor(Color.BLUE);
-        int w = mWidth / 8;
-        int h = mHeight / 8;
-        for (int x = 0; x < mWidth; x += 2 * w) {
-            for (int y = 0; y < mHeight; y += 2 * h) {
-                canvas.drawRect(x, y, x + w, y + h, paint);
+        if (mPieceImages[1] == null && !mInitializeFailed) {
+            initializeImages();
+        }
+        if (!mInitializeFailed) {
+            int w = mWidth / 8;
+            int h = mHeight / 8;
+            boolean flippy = false;
+            for (int y = 0; y < 8; y++) {
+                for (int x = 0; x < 8; x++) {
+                    int charIndex = y * 8 + x;
+                    int rX = x * w;
+                    int rY = y * h;
+                    if (flippy) {
+                        canvas.drawRect(rX - 5, rY - 5, rX + w, rY + h, paintBlack);
+                    } else {
+                        canvas.drawRect(rX - 5, rY - 5, rX + w, rY + h, paintWhite);
+                    }
+                    int pieceType = getPieceTypeFromCharIndex(charIndex);
+                    if (pieceType != 0) {
+                        canvas.drawBitmap(mPieceImages[pieceType], rX, rY, paintTransparent);
+                    }
+                    flippy = !flippy;
+                }
+                flippy = !flippy;
             }
         }
-        canvas.drawText("Test", 0, 100, paint);
     }
+
 }
